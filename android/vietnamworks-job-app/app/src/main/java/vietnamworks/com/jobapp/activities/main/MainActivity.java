8d2d52bc6e59fa3b534a0709fb2cd0ad2a91e26a
Nguyen Khanh Duy <ricky.ngk@gmail.com>
2016-02-23 10:44:34 +0700

    search form

--- android/vietnamworks-job-app/app/src/main/java/vietnamworks/com/jobapp/activities/main/MainActivity.java
+++ android/vietnamworks-job-app/app/src/main/java/vietnamworks/com/jobapp/activities/main/MainActivity.java
@@ -22,43 +22,30 @@ import android.view.View;
 
 import R.helper.BaseActivity;
 import R.helper.BaseFragment;
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import vietnamworks.com.jobapp.R;
 import vietnamworks.com.jobapp.activities.main.fragments.ExploreFragment;
 import vietnamworks.com.jobapp.activities.main.fragments.SearchFragment;
 import vietnamworks.com.jobapp.activities.main.fragments.UserJobsFragment;
 
 public class MainActivity extends BaseActivity {
-
-    /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-     */
     private SectionsPagerAdapter mSectionsPagerAdapter;
 
-    /**
-     * The {@link ViewPager} that will host the section contents.
-     */
-    private ViewPager mViewPager;
+    @Bind(R.id.tabs) TabLayout tabLayout;
+    @Bind(R.id.container) ViewPager mViewPager;
+    @Bind(R.id.toolbar) Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
+
         mSectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
-
-        // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.container);
         mViewPager.setAdapter(mSectionsPagerAdapter);
-
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(mViewPager);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -69,6 +56,14 @@ public class MainActivity extends BaseActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    public void hideTabs() {
+        tabLayout.setVisibility(View.GONE);
+    }
+
+    public void showTabs() {
+        tabLayout.setVisibility(View.VISIBLE);
     }
 
 
@@ -98,7 +93,6 @@ public class MainActivity extends BaseActivity {
         BaseFragment tmp = mSectionsPagerAdapter.getFragment(0);
         if (tmp != null) {
             SearchFragment f = (SearchFragment)tmp;
-            f.openSearchResult();
         }
     }
 
