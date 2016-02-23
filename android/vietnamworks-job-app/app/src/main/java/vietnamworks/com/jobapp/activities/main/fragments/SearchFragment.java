7fe66eed62cf0d3f108fe7a2605bc642fd922aee
Nguyen Khanh Duy <ricky.ngk@gmail.com>
2016-02-23 11:44:14 +0700

    cont working on extras search form (not completed)

--- android/vietnamworks-job-app/app/src/main/java/vietnamworks/com/jobapp/activities/main/fragments/SearchFragment.java
+++ android/vietnamworks-job-app/app/src/main/java/vietnamworks/com/jobapp/activities/main/fragments/SearchFragment.java
@@ -4,7 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
+import android.widget.Spinner;
 
 import R.helper.BaseFragment;
 import butterknife.Bind;
@@ -22,11 +24,50 @@ public class SearchFragment extends BaseFragment {
     @Bind(R.id.btn_search)
     ImageButton btnSearch;
 
+    @Bind(R.id.select_industry)
+    Spinner selectIndustry;
+
+    @Bind(R.id.select_working_place)
+    Spinner selectWorkingPlace;
+
+    @Bind(R.id.select_min_salary)
+    Spinner selectMinSalary;
+
+    @Bind(R.id.view_search_extra)
+    View searchExtraView;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_search, container, false);
         ButterKnife.bind(this, rootView);
+
+        importSelect(selectIndustry, R.array.array_industry);
+        importSelect(selectWorkingPlace, R.array.array_locations);
+        importSelect(selectMinSalary, R.array.array_min_salary);
+
+        searchExtraView.setVisibility(View.GONE);
+
+        btnAdvanceSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (searchExtraView.getVisibility() == View.GONE) {
+                    searchExtraView.setVisibility(View.VISIBLE);
+
+                }
+            }
+        });
+
         return rootView;
+    }
+
+    private void resetExtraForm() {
+        //selectWorkingPlace.select
+    }
+
+    private void importSelect(Spinner target, int resId) {
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this.getContext(), resId, R.layout.cv_dropdown_simple);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        target.setAdapter(adapter);
     }
 }
