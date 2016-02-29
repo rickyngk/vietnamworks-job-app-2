76019deaf5d7dc920dfa1303525d178ee320fe10
Nguyen Khanh Duy <ricky.ngk@gmail.com>
2016-02-29 09:47:41 +0700

    show tab title

--- android/vietnamworks-job-app/app/src/main/java/vietnamworks/com/jobapp/activities/main/MainActivity.java
+++ android/vietnamworks-job-app/app/src/main/java/vietnamworks/com/jobapp/activities/main/MainActivity.java
@@ -159,16 +159,9 @@ public class MainActivity extends BaseActivity {
 
         @Override
         public CharSequence getPageTitle(int position) {
-            /*
             Drawable image = ContextCompat.getDrawable(ctx, imageResId[position]);
-            image.setBounds(0, 0, image.getIntrinsicWidth()*3/4, image.getIntrinsicHeight()*3/4);
+            image.setBounds(0, 0, image.getIntrinsicWidth()*4/5, image.getIntrinsicHeight()*4/5);
             SpannableString sb = new SpannableString(" \n" + sections[position]);
-            ImageSpan imageSpan = new ImageSpan(image, ImageSpan.ALIGN_BASELINE);
-            sb.setSpan(imageSpan, 0, 1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-            return sb;*/
-            Drawable image = ContextCompat.getDrawable(ctx, imageResId[position]);
-            image.setBounds(0, 0, image.getIntrinsicWidth(), image.getIntrinsicHeight());
-            SpannableString sb = new SpannableString(" ");
             ImageSpan imageSpan = new ImageSpan(image, ImageSpan.ALIGN_BASELINE);
             sb.setSpan(imageSpan, 0, 1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
             return sb;
